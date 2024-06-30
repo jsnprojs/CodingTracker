269b5bbe044b26e21064a3c7c019a56eca24bbad
kang yi fan <jason3631061@gmail.com>
2024-06-30 13:05:58 -0700

    add simple menu prompt

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,9 +1,12 @@
 ﻿namespace CodingTracker;
 
+
 internal class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Menu menu = new();
+        menu.GetUserInput();
     }
 }
