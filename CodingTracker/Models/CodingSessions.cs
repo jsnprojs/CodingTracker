cd199dce8abfcab52f4164c2b7d8edc8480dbb2f
kang yi fan <jason3631061@gmail.com>
2024-07-07 19:35:46 -0700

    Add CodingSession.cs model

---
+++ CodingTracker/Models/CodingSessions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTracker.Models;
+internal class CodingSessions
+{
+    internal int ID { get; set; }
+    internal DateTime StartTime { get; set; }
+    internal DateTime EndTime { get; set; }
+    internal int Duration { get; set; }
+}
--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -6,6 +6,6 @@ internal class Program
 {
     static void Main(string[] args)
     {
-         
+        string connectionString = ConfigurationManager.AppSettings.Get("connectionString");
     }
 }
